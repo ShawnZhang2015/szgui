2fa64086b49c2ceade5ec7ef8ee864715b831b80
zjzMS <tongyonyouxiang@163.com>
2016-11-22 09:58:39 +0800

    修改Toggle控件

--- assets/gui/Toggle/Toggle.js
+++ assets/gui/Toggle/Toggle.js
@@ -2,44 +2,60 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-      
-        isChecked: {
-            set: function(value) {
-                if (!this.switchOn || !this.switchOff || !this.switchThumb) {
-                    return;
-                }
-                
-                this.switchOn.active = value;
-                this.switchOff.active = !value;
-                
-                var pt = this.switchThumb.getPosition();
-                pt.x = value ? (this.node.width - this.switchThumb.width) / 2 - 4 
-                             : (this.switchThumb.width - this.node.width) / 2 + 4;
-                this.switchThumb.position = pt;   
-                // this.scheduleOnce(()=>{
-                // this.switchThumb.runAction(cc.moveTo(0.1, pt));
-                // });
-                
-            },
-
-            get: function() {
-                if (!this.switchOn) {
-                    return false;
-                }
-                return this.switchOn.active;    
+        
+        isChecked:{
+            default: false,
+            type: Boolean,
+            notify: function() {
+                this._updateStatus();
             },
         },
     },
 
-    
+    _initNode: function() {
+        if (!this.switchOn) {
+            this.switchOn = this.node.getChildByName('switchOn');    
+        }
+
+        if (!this.switchOff) {
+            this.switchOff = this.node.getChildByName('switchOff');    
+        }
+
+        if (!this.switchThumb ) {
+            this.switchThumb = this.node.getChildByName('switchThumb');    
+        } 
+    },
+
+    _updateStatus: function() {
+        if (this._isSwitching) {
+            return;
+        }
+        this._isSwitching = true;
+
+        this._initNode();      
+        
+        this.switchOn.active = true;
+        this.switchOff.active = true;
+     
+        var pt1 = this.switchThumb.getPosition();
+        pt1.x = this.isChecked  ? (this.node.width - this.switchThumb.width) / 2 
+                                : (this.switchThumb.width - this.node.width) / 2;
+
+        var pt2 = cc.p(this.isChecked ? -4 : 4, pt1.y);
+        var callfunc = cc.callFunc(()=>{
+            this.switchOn.active = this.isChecked;
+            this.switchOff.active = !this.isChecked;
+        });
+        var sequence = cc.sequence(cc.moveTo(0.05, pt1), callfunc, cc.moveBy(0.2, pt2), cc.callFunc(()=>{
+            this._isSwitching = false;
+        }));               
+        this.switchThumb.runAction(sequence);
+    },
 
     // use this for initialization
     onLoad: function () {
-        this.switchOn = this.node.getChildByName('switchOn');
-        this.switchOff = this.node.getChildByName('switchOff');
-        this.switchThumb = this.node.getChildByName('switchThumb');
-
-        this.isChecked = false;
+        this._initNode();
+        this._isSwitching = false;
         //点击后切换
         this.switchThumb.on(cc.Node.EventType.TOUCH_END, () => {
             this.isChecked = !this.isChecked;
